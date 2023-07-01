aa24672ce8351ed78a2e24c7c119241a59fd724b
Rahul Parkar <rahul.a.parkar@gmail.com>
2023-07-01 16:17:41 +0100

    Update getter formatting

--- src/dataFetchers/getVatsimData/getData.spec.ts
+++ src/dataFetchers/getVatsimData/getData.spec.ts
@@ -1,13 +1,17 @@
 import nock from "nock";
-import { handler } from "./index";
+import { getData } from "./getData";
 
 describe("getVatsimData", () => {
   it("should call vatsim to get the data", async () => {
     const nockMock = nock("https://data.vatsim.net")
       .get("/v3/vatsim-data.json")
-      .reply(200, {})
+      .reply(200, {
+        test: "test",
+      })
       .persist();
-    await handler();
-    expect(nockMock.isDone()).toBeTrue();
+    const response = await getData();
+    expect(response).toBe({
+      test: "test",
+    });
   });
 });
--- src/dataFetchers/getVatsimData/index.ts
+++ src/dataFetchers/getVatsimData/index.ts
@@ -1,11 +1,3 @@
-import got from "got";
+import { getData } from "./getData";
 
-export const handler = async (): Promise<void> => {
-  const data = await got
-    .get("https://data.vatsim.net/v3/vatsim-data.json")
-    .json();
-
-  console.log(data);
-};
-
-handler();
+getData();

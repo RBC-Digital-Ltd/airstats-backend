970dc882a999a8aceb57c39c6b557d636b85e4a6
Rahul Parkar <rahul.a.parkar@gmail.com>
2023-07-01 17:36:53 +0100

    Add rabbit mq publishing

--- src/dataFetchers/getVatsimData/index.ts
+++ src/dataFetchers/getVatsimData/index.ts
@@ -1,8 +1,21 @@
+import { Connection } from "rabbitmq-client";
 import { getData } from "./getData.js";
 
 const main = async () => {
   const data = await getData();
   console.log(data);
+
+  const rabbit = new Connection("amqp://localhost");
+  const pub = rabbit.createPublisher({
+    confirm: true,
+    maxAttempts: 2,
+    exchanges: [{ exchange: "my-events", type: "topic" }],
+  });
+
+  await pub.send("vatsim-data", data);
+
+  await pub.close();
+  await rabbit.close();
 };
 
 main();

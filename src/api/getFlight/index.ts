d248047ac98c71f758d0da187d648c28b68f2662
Rahul Parkar <rahul.a.parkar@gmail.com>
2023-07-02 02:05:32 +0100

    Add queryable parameter

--- src/api/getFlight/index.ts
+++ src/api/getFlight/index.ts
@@ -1,14 +1,16 @@
-import type { APIGatewayProxyResult } from "aws-lambda";
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { query } from "../../db/client";
 
-export const handler = async (): Promise<APIGatewayProxyResult> => {
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   const dbQuery = await query(
     `select f.*, json_agg(fd.* order by fd."timestamp" asc) as fd
   from flight f
   join flightdata fd on fd.flight_id = f.id
   where f.id = $1
   group by f.id`,
-    [137]
+    [event.pathParameters?.id]
   );
 
   return {
